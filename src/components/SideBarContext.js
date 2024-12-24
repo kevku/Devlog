59b033c74507ed3adb1a97ee776ef1890d4801af
kevk <kevinku668833@gmail.com>
2024-12-24 04:23:29 -0800

    Gave details of what SideBarContext does

--- src/components/SideBarContext.js
+++ src/components/SideBarContext.js
@@ -1,3 +1,11 @@
+/**
+ * Used to communicate the Header Hamburger button to the Sidebar
+ * Hamburger button is in the Header which is in the Layout, but
+ * Sidebar is only in Private Layout
+ * 
+ * This creates a provider to encapsulate Layout
+ * Then both header and Sidebar can coordinate
+ */
 import React, { createContext, useState, useContext } from 'react';
 
 const SideBarContext = createContext();
