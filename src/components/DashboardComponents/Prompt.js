30bf6e832d360935af82ab32449230a782f7a0fc
unknown <kekuang5@gmail.com>
2025-05-02 02:32:22 -0700

    Changed the styling of prompts

--- src/components/DashboardComponents/Prompt.js
+++ src/components/DashboardComponents/Prompt.js
@@ -1,19 +1,31 @@
+import { MdDelete, MdEdit } from "react-icons/md";
 import styles from "../../styles/DashboardPage/Prompt.module.css";
 
 const Prompt = ({ prompt, onClick, onDelete }) => {
   return (
     <div className={styles.prompt} onClick={() => onClick(prompt.id)}>
+      <div className={styles.buttonContainer}>
+        <button
+          className={`${styles.iconButton} ${styles.editButton}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            // Edit functionality will be added later
+          }}
+        >
+          <MdEdit />
+        </button>
+        <button
+          className={`${styles.iconButton} ${styles.deleteButton}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete(prompt.id);
+          }}
+        >
+          <MdDelete />
+        </button>
+      </div>
       <h3 className={styles.promptTitle}>{prompt.title}</h3>
       <p className={styles.promptDescription}>{prompt.description}</p>
-      <button
-        className={styles.deleteButton}
-        onClick={(e) => {
-          e.stopPropagation(); // Prevent the click event from triggering onClick on parent div
-          onDelete(prompt.id);
-        }}
-      >
-        X
-      </button>
     </div>
   );
 };
